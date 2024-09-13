07e9bffed0a6d3571b3d80da7a3a6ee8eb99cee5
Muhammad Anas Khan <muhammadanaskhan0212@gmail.com>
2024-09-13 22:14:51 +0500

    After Change

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -11,12 +11,21 @@ function PostList(params) {
 
     useEffect( () => {
         setfetching(true);
-        fetch('https://dummyjson.com/posts')
+
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        fetch('https://dummyjson.com/posts', { signal })
         .then(res => res.json())
         .then(data => {
             addInitialPosts(data.posts);
             setfetching(false);
         });
+
+        return () => {
+            console.log("Cleaning the useEffect while the componenent is removing from the DOM");
+            controller.abort();
+        }
         
     }, [])
 
